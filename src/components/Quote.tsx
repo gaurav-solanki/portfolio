49c8602e55b43766db42320a748c1d0a8e2b4086
gaurav-solanki <gaurav.solanki2212@gmail.com>
2024-05-21 01:20:30 +0530

    build error fixed

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../main";
 import { fetchRandomQuote } from "../slices/quoteSlice";
-import { AppDispatch, RootState } from "../main"; 
 
 const Quote = () => {
     const dispatch = useDispatch<AppDispatch>();
